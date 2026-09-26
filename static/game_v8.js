3ab07ac02e000af652a0c61de1cb27f0fe3fb0c6
tokentalestw-cloud <tokentalestw@gmail.com>
2026-09-26 17:13:10 +0800

    Update tutorial: cycle through 5 newly uploaded Yuki-onna images

--- static/game_v8.js
+++ static/game_v8.js
@@ -29372,7 +29372,10 @@ window.XLW_Tutorial = {
     if (sprite) {
         const images = [
             { src: "/static/card_images/yuki_tutorial_1.jpg", clip: "none", width: "120px", height: "160px", transform: "translateY(-10px)" },
-            { src: "/static/card_images/yuki_tutorial_2.jpg", clip: "none", width: "120px", height: "160px", transform: "translateY(-10px)" }
+            { src: "/static/card_images/yuki_tutorial_2.jpg", clip: "none", width: "120px", height: "160px", transform: "translateY(-10px)" },
+            { src: "/static/card_images/yuki_tutorial_3.jpg", clip: "none", width: "120px", height: "160px", transform: "translateY(-10px)" },
+            { src: "/static/card_images/yuki_tutorial_4.jpg", clip: "none", width: "120px", height: "160px", transform: "translateY(-10px)" },
+            { src: "/static/card_images/yuki_tutorial_5.jpg", clip: "none", width: "120px", height: "160px", transform: "translateY(-10px)" }
         ];
         const imgData = images[this.currentStep % images.length];
         sprite.src = imgData.src;
